93f3033af068f21317befbb6ed1e9ff3b9aabcea
Sergio Llanos <sergio.llanos.garcia@gmail.com>
2024-03-21 10:19:15 -0500

    Add some styles and a react hook to control the screen size

--- app/components/LeftColumn/LeftColumn.tsx
+++ app/components/LeftColumn/LeftColumn.tsx
@@ -1,7 +1,26 @@
 import { Button } from "@shopify/polaris";
+import styles from "./LeftColumn.module.css";
 
-const LeftColumn = () => {
-  return <Button variant="primary">Botón de Polaris</Button>;
+const LeftColumn = ({
+  smallScreen,
+  showPolaris,
+  setShowPolaris,
+}: {
+  smallScreen: boolean;
+  showPolaris: boolean;
+  setShowPolaris: (showPolaris: boolean) => void;
+}) => {
+  return (
+    <>
+      {(!smallScreen || showPolaris) && (
+        <div className={styles.leftSection}>
+          <Button onClick={() => setShowPolaris(false)} variant="primary">
+            Botón de Polaris
+          </Button>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default LeftColumn;
--- app/components/Middle/Middle.tsx
+++ app/components/Middle/Middle.tsx
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import styles from "./Middle.module.css";
 
 const StyledDiv = styled.div<{ inputstyles: string }>`
   ${({ inputstyles }) => `${inputstyles}`}
@@ -6,13 +7,15 @@ const StyledDiv = styled.div<{ inputstyles: string }>`
 
 const Middle = ({ inputStyles }: { inputStyles: string }) => {
   return (
-    <StyledDiv inputstyles={inputStyles}>
-      <h2 className="title">Lorem ipsum dolor sit amet</h2>
-      <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt u</p>
-      <button type="button" className="btn btn-primary">
-        Est laborum
-      </button>
-    </StyledDiv>
+    <div className={styles.middleSection}>
+      <StyledDiv inputstyles={inputStyles}>
+        <h2 className="title">Lorem ipsum dolor sit amet</h2>
+        <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt u</p>
+        <button type="button" className="btn btn-primary">
+          Est laborum
+        </button>
+      </StyledDiv>
+    </div>
   );
 };
 
